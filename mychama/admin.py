e57d204e38582d8086e882450168bacbf11c9d4b
johnwanjema <jonwanjema@gmail.com>
2019-08-15 15:34:59 +0300

    added profile form its urls and added models to admin

--- mychama/admin.py
+++ mychama/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Individual,Group,Group_member,Group_contribution,Individual_contribution
 
 # Register your models here.
+admin.site.register(Individual)
+admin.site.register(Group)
+admin.site.register(Group_member)
+admin.site.register(Group_contribution)
+admin.site.register(Individual_contribution)
--- mychama/urls.py
+++ mychama/urls.py
@@ -1,7 +1,13 @@
 from django.conf.urls import url,include
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns=[
     url('^$',views.index,name = 'index'),
-    
-    ]
+    url(r'^profile/edit$',views.update_profile,name='edit'),
+    url('profile/', views.index, name='profile'),
+]
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
